e0f5dfc13ce91d80e4fc2cc070e06e464a92b7de
tomo25 <tomotomo2525neko@gmail.com>
2024-12-01 14:29:17 +0900

    MODS.TOMLの更新

--- src/main/java/org/new_artemis/Reference.java
+++ src/main/java/org/new_artemis/Reference.java
@@ -5,7 +5,7 @@ public class Reference
     //Essentials
     public static final String MODID = "newartemis";  // MODIDを新しいものに変更
     public static final String NAME = "New Artemis";  // MOD名を変更
-    public static final String VERSION = "1.0.6";  // バージョンはそのまま
+    public static final String VERSION = "1.0.0";  // バージョンはそのまま
     public static final String ACCEPTEDVERSIONS = "[1.18.2]";  // 1.18.2に変更
     public static final String DEPENDENCIES = "required-after:forge@[40.2.0,)";  // Forgeのバージョンを変更
 
